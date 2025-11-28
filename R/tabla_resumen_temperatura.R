f2b69910c6ec46d3720bcd1098772b29a0d718df
Valentin Monteverde <monteverdevalentin@gmail.com>
2025-11-28 07:09:36 -0300

    corrijo doc y ejemplo de tabla_resumen_temperatura

--- R/tabla_resumen_temperatura.R
+++ R/tabla_resumen_temperatura.R
@@ -1,6 +1,8 @@
-#' Tabla resumen de temperatura por estacion
+#' @title Tabla resumen de temperatura por estación
+#' @name tabla_resumen_temperatura
+#' @aliases tabla_resumen_temperatura
 #'
-#' Calcula la media y el desvio estandar de la temperatura de abrigo
+#' Calcula la media y el desvío estándar de la temperatura de abrigo
 #' a 150 cm para un conjunto de estaciones.
 #'
 #' @param datos data.frame con los datos (debe tener columnas `id`
@@ -8,9 +10,16 @@
 #' @param estaciones vector de ids de estaciones a incluir.
 #'
 #' @return data.frame con columnas: id, promedio, desviacion.
+#'
 #' @importFrom dplyr %>% mutate group_by summarise
+#'
 #' @examples
-#' tabla_resumen_temperatura(datos_ejemplo)
+#' data("datos_ejemplo")
+#' tabla_resumen_temperatura(
+#'   datos_ejemplo,
+#'   estaciones = c("NHO472", "NHO437")
+#' )
+#'
 #' @export
 tabla_resumen_temperatura <- function(datos, estaciones) {
   datos %>%
